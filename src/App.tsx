fc5e588cf854f61180160d169f9ff5799a5489c5
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-24 11:23:14 -0400

    modification this files

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,23 @@ function App() {
   const theme = useMemo( () => createTheme(themeSettings), [] )
 
   return <div className='app'> 
-
-  <ThemeProvider theme={theme}>
-
-    <CssBaseline/>
-     <Box width={'100%'}
-     height={"100%"}
-     padding={" 1rem 2rem 4rem 2 rem"}
-     >
-      <Routes>
-        <Route  path='/' element={ <div> Dashboard page </div>}/> 
-        <Route path='/predictions' element={<div> Predication page </div>}/>
-      </Routes>
-     </Box>
-  </ThemeProvider>
-  
-  
-   </div>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+         <Box  
+         width={"100%"}
+         height={'100%'}
+         padding={'1rem 2rem 4rem 2 rem '}
+         >
+            <Routes >
+               <Route path='/' element={<div> Dashboard Page</div>}/>
+               <Route path='/predictions' element={ <div> Prediction Page</div>}/>
+            </Routes>
+         </Box>
+    </ThemeProvider>
+  </BrowserRouter>
+ 
+ </div>
 }
 
 export default App
