b04dc3c9074d1385fbc5881370622607e77e83fb
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-24 10:44:46 -0400

    React Router and Navigaton

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import { ThemeProvider, createTheme } from '@mui/material';
 import { useMemo } from 'react';
 import {  themeSettings} from './theme';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Box } from "@mui/material"
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
 
 function App() {
 
   const theme = useMemo( () => createTheme(themeSettings), [] )
-
-
-
 
   return <div className='app'> 
 
   <ThemeProvider theme={theme}>
 
     <CssBaseline/>
-
+     <Box width={'100%'}
+     height={"100%"}
+     padding={" 1rem 2rem 4rem 2 rem"}
+     >
+      <Routes>
+        <Route  path='/' element={ <div> Dashboard page </div>}/> 
+        <Route path='/predictions' element={<div> Predication page </div>}/>
+      </Routes>
+     </Box>
   </ThemeProvider>
   
   
