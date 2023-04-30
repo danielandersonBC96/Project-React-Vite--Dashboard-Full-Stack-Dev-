656cc081f87e4d106cea217017b8af6905acca1a
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-30 13:23:32 -0400

    create dashboard UI part 1

--- src/Pages/Dashboard/Row1.tsx
+++ src/Pages/Dashboard/Row1.tsx
@@ -9,13 +9,33 @@ YAxis,
 Tooltip,
 Area
 
-} from 'recharts'
+} from 'recharts';
+import{ useMemo } from 'react';
+import {useTheme} from '@mui/material';
+
 
 
 const Row1 = () => {
-
+    const{ palette } = useTheme();
     const {data} = useGetKpisQuery()
     console.log('data', data )
+    
+    const revenueExpenses = useMemo(() => {
+        return (
+            data &&
+            data[0].monthlyData.map(({month, revenue, expenses})=>{
+                return {
+                    name: month.substring(0,3),
+                    revenue:revenue,
+                    expenses:expenses
+
+
+
+                }
+            })
+        )
+    }, [data])
+
 
     return (
 
@@ -25,7 +45,7 @@ const Row1 = () => {
                 <AreaChart
                 width={500}
                 height={400}
-                data={data}
+                data={revenueExpenses}
                 margin={
                     {
                         top:10,
@@ -36,10 +56,64 @@ const Row1 = () => {
                 }
                 >
                     <CartesianGrid strokeDasharray={"3 3"}/>
-                    <XAxis dataKey={"name"}/>
-                    <YAxis/>
+                    <XAxis dataKey={"name"}
+                    tickLine={false}
+                    style={{
+                        fontSize: "10px"
+                    }}
+
+                    />
+                    <YAxis
+                    tickLine={false}
+                    axisLine={
+                        {
+                            strokeWidth: "0"
+                        }
+                    }
+                    style={{
+                        fontSize: "10px"
+                    }} 
+                    domain={[
+                        8000,
+                        23000
+
+                    ]}
+                    />
+                        <defs>
+                        <linearGradient
+                        id="colorRevenue"
+                        x1="0"
+                        y1="0"
+                        x2="0"
+                        y2="1"
+                        >
+                            <stop 
+                            offset="5%"
+                            stopColor={palette.primary[300]}
+                            stopOpacity={0.5}
+                            />
+                              <stop 
+                            offset="95%"
+                            stopColor={palette.primary[300]}
+                            stopOpacity={0}
+                            />
+
+                        </linearGradient>
+
+                      </defs>
                     <Tooltip/>
-                    <Area type={"monotone"} dataKey={"uv"} stroke='#8884d8' fill='#8884d8'/>
+                    <Area type={"monotone"}
+                     dataKey={"revenue"} 
+                     dot={true}
+                     stroke={palette.primary.main} 
+                     fillOpacity={1}
+                     fill='url(#colorRevenue)'/>
+                      <Area type={"monotone"}
+                     dataKey={"expenses"} 
+                     dot={true}
+                     stroke={palette.primary.main} 
+                     fillOpacity={1}
+                     fill='url(#colorExpenses)'/>
                 </AreaChart>  
              </ResponsiveContainer>
            </DashboardBox>
---
+++ src/states/types.ts
@@ -0,0 +1,35 @@
+
+export interface ExpensesByCategory{
+    salaries: number;
+    supplines:number;
+    services: number;
+}
+
+export interface Month {
+    id: string;
+    month: string;
+    revenue:  number;
+    expenses:number;
+    monOperationExpenses: number;
+    operationalExpense: number ;
+
+}
+
+export interface Day {
+    id:  string;
+    data : string;
+    revenue: string;
+    expenses: number; 
+
+}
+
+export interface GetKpisResponse {
+    id: string;
+   _id: string;
+   __v: number;
+   totalProfit: number;
+   totalRevenue: number;
+   expenseByCategory: ExpensesByCategory;
+   monthlyData: Array<Month>;
+   dalyData:Array<Day>
+}
