8d27c90dd1f8007acd8b90727ffaaf38fdeed428
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-05-19 16:36:27 -0400

    finhs prject

--- src/Pages/predictions/index.tsx
+++ src/Pages/predictions/index.tsx
@@ -2,9 +2,10 @@ import DashboardBox from '@/Components/DashboardBox';
 import FlexBetween from '@/Components/FlexBetween.';
 import { useGetKpisQuery } from '@/states/Api';
 import { Box,Button,Typography, useTheme } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useMemo,useState } from 'react';
 import {
      CartesianGrid, 
+     Label, 
      Legend, 
      Line, 
      LineChart, 
@@ -12,7 +13,7 @@ import {
      Tooltip, 
      XAxis, 
      YAxis } from 'recharts';
-
+     import regression, { DataPoint } from 'regression';
 
 type Props = {}
 
@@ -22,89 +23,101 @@ const  Predications = (props: Props) => {
     const [ isPredictions, setIsPredictions ] = useState(false);
     const { data : kpiData} = useGetKpisQuery()
 
+    const formattedData = useMemo(() => {
+        if (!kpiData) return [];
+        const monthData = kpiData[0].monthlyData;
+    
+        const formatted: Array<DataPoint> = monthData.map(
+          ({ revenue }, i: number) => {
+            return [i, revenue];
+          }
+        );
+        const regressionLine = regression.linear(formatted);
+    
+        return monthData.map(({ month, revenue }, i: number) => {
+          return {
+            name: month,
+            "Actual Revenue": revenue,
+            "Regression Line": regressionLine.points[i][1],
+            "Predicted Revenue": regressionLine.predict(i + 12)[1],
+          };
+        });
+      }, [kpiData]);
+
   return (
-
-    <DashboardBox
-
-    width={'100%'}
-    height={'100%'}
-    p={'1rem'}
-    overflow={'hidden'}
-    >
-        <FlexBetween m='1rem 2.5rem' gap='0.3rem'>
-            <Box>
-                <Typography variant='h3'>
-                    Revenue and Predications
-                </Typography>
-                <Typography variant='h6'>
-                    Revenue and predicated  base on a simple linear regression model 
-                </Typography>
-            </Box>
-            <Button
-            onClick={() => setIsPredictions(!isPredictions)}
-            sx={{
-                color:palette.grey[900],
-                boxShadow: "0.1rem 0.1rem 0.1rem rgba(0,0,0,.4)",
-                backgroundColor: palette.grey[700],
-
-            }}
-            >
-             Show Predicted Revenue for next Year 
-
-            </Button>
-          
-
-
-        </FlexBetween>
-
-        <ResponsiveContainer  width={"100%"} height={"100%"}>
-                <LineChart
-                margin={
-                    {
-                        top:15,
-                        right:0,
-                        left:-10,
-                        bottom:55,
-                    }
-                }
-                >
-                    <CartesianGrid vertical={false} stroke={palette.grey[800]}/>
-                    <XAxis dataKey={"name"}
-                    tickLine={false}
-                    style={{fontSize: "10px"}}
-
-                    />
-                    <YAxis
-                    yAxisId={"left"}
-                    tickLine={false}
-                    axisLine={false}
-                    style={{fontSize: "10px"}} 
-                    />
-                    
-                    <Tooltip/>
-                    <Legend
-                    height={20} wrapperStyle={{
-                     margin: ' 0 0 10px 0 '
-                    }}
-                    />
-                    <Line
-                    yAxisId={"left"}
-                    type={"monotone"}
-                    dataKey={"profit"}
-                    stroke={palette.tertiary[500]}
-                    
-                    />
-                      <Line
-                      
-                      yAxisId={"left"}
-                      type={"monotone"}
-                      dataKey={"revenue"}
-                      stroke={palette.primary[500]}
-                      
-                      />
-                </LineChart>  
-             </ResponsiveContainer>
-    </DashboardBox>
+    <DashboardBox width="100%" height="100%" p="1rem" overflow="hidden">
+    <FlexBetween m="1rem 2.5rem" gap="1rem">
+      <Box>
+        <Typography variant="h3">Revenue and Predictions</Typography>
+        <Typography variant="h6">
+          charted revenue and predicted revenue based on a simple linear
+          regression model
+        </Typography>
+      </Box>
+      <Button
+        onClick={() => setIsPredictions(!isPredictions)}
+        sx={{
+          color: palette.grey[900],
+          backgroundColor: palette.grey[700],
+          boxShadow: "0.1rem 0.1rem 0.1rem 0.1rem rgba(0,0,0,.4)",
+        }}
+      >
+        Show Predicted Revenue for Next Year
+      </Button>
+    </FlexBetween>
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        data={formattedData}
+        margin={{
+          top: 20,
+          right: 75,
+          left: 20,
+          bottom: 80,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" stroke={palette.grey[800]} />
+        <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }}>
+          <Label value="Month" offset={-5} position="insideBottom" />
+        </XAxis>
+        <YAxis
+          domain={[12000, 26000]}
+          axisLine={{ strokeWidth: "0" }}
+          style={{ fontSize: "10px" }}
+          tickFormatter={(v) => `$${v}`}
+        >
+          <Label
+            value="Revenue in USD"
+            angle={-90}
+            offset={-5}
+            position="insideLeft"
+          />
+        </YAxis>
+        <Tooltip />
+        <Legend verticalAlign="top" />
+        <Line
+          type="monotone"
+          dataKey="Actual Revenue"
+          stroke={palette.primary.main}
+          strokeWidth={0}
+          dot={{ strokeWidth: 5 }}
+        />
+        <Line
+          type="monotone"
+          dataKey="Regression Line"
+          stroke="#8884d8"
+          dot={false}
+        />
+        {isPredictions && (
+          <Line
+            strokeDasharray="5 5"
+            dataKey="Predicted Revenue"
+            stroke={palette.secondary[500]}
+          />
+        )}
+      </LineChart>
+    </ResponsiveContainer>
+  </DashboardBox>
+   
   )
 
 
