a4264f12e485d0353474a44987121cd8c1b04111
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-05-19 15:04:57 -0400

    creat pad predictions layout and grap

--- src/Pages/predictions/index.tsx
+++ src/Pages/predictions/index.tsx
@@ -3,6 +3,15 @@ import FlexBetween from '@/Components/FlexBetween.';
 import { useGetKpisQuery } from '@/states/Api';
 import { Box,Button,Typography, useTheme } from '@mui/material';
 import React, { useState } from 'react';
+import {
+     CartesianGrid, 
+     Legend, 
+     Line, 
+     LineChart, 
+     ResponsiveContainer, 
+     Tooltip, 
+     XAxis, 
+     YAxis } from 'recharts';
 
 
 type Props = {}
@@ -22,7 +31,7 @@ const  Predications = (props: Props) => {
     p={'1rem'}
     overflow={'hidden'}
     >
-        <FlexBetween m='1rem 2.5rem'>
+        <FlexBetween m='1rem 2.5rem' gap='0.3rem'>
             <Box>
                 <Typography variant='h3'>
                     Revenue and Predications
@@ -35,15 +44,66 @@ const  Predications = (props: Props) => {
             onClick={() => setIsPredictions(!isPredictions)}
             sx={{
                 color:palette.grey[900],
-                boxShadow: "0.1rem 0.1rem 0.1rem rgba(0,0,0,.4",
-                backgroundColor: palette.grey[700]
+                boxShadow: "0.1rem 0.1rem 0.1rem rgba(0,0,0,.4)",
+                backgroundColor: palette.grey[700],
 
             }}
             >
-             
+             Show Predicted Revenue for next Year 
 
             </Button>
+          
+
+
         </FlexBetween>
+
+        <ResponsiveContainer  width={"100%"} height={"100%"}>
+                <LineChart
+                margin={
+                    {
+                        top:15,
+                        right:0,
+                        left:-10,
+                        bottom:55,
+                    }
+                }
+                >
+                    <CartesianGrid vertical={false} stroke={palette.grey[800]}/>
+                    <XAxis dataKey={"name"}
+                    tickLine={false}
+                    style={{fontSize: "10px"}}
+
+                    />
+                    <YAxis
+                    yAxisId={"left"}
+                    tickLine={false}
+                    axisLine={false}
+                    style={{fontSize: "10px"}} 
+                    />
+                    
+                    <Tooltip/>
+                    <Legend
+                    height={20} wrapperStyle={{
+                     margin: ' 0 0 10px 0 '
+                    }}
+                    />
+                    <Line
+                    yAxisId={"left"}
+                    type={"monotone"}
+                    dataKey={"profit"}
+                    stroke={palette.tertiary[500]}
+                    
+                    />
+                      <Line
+                      
+                      yAxisId={"left"}
+                      type={"monotone"}
+                      dataKey={"revenue"}
+                      stroke={palette.primary[500]}
+                      
+                      />
+                </LineChart>  
+             </ResponsiveContainer>
     </DashboardBox>
   )
 
